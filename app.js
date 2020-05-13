880a3f55281c4dae239f9aece4b7e49260c506e2
Glen D'mello <meldmello60@gmail.com>
2020-05-13 14:46:17 -0700

    -rough-master and finl UI merged

--- app.js
+++ app.js
@@ -9,15 +9,9 @@ const userData =  require("./data/login");
 const session = require('express-session');
 const { exec } = require('child_process');
 const mongoose = require('mongoose')
+const Admin = mongoose.mongo.Admin;
 var xss = require("xss");
-const reviews = require('./data/review');
-const cookies = require('cookie-parser');
 
-
-// mongoose.connect('mongodb://localhost:27017/Gadget_Clear', function(){
-//     /* Drop the DB */
-//     mongoose.connection.db.dropDatabase();
-// });
 
 const handlebarsInstance = exphbs.create({
 	defaultLayout: 'main'
@@ -27,7 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded(
   {extended:true}
 ));
-app.use(cookies());
 
 app.use(session({
 	key:"AuthCookie",
@@ -157,8 +150,7 @@ app.post("/login",async(req,res)=>{
 			});
 		}
 		else {
-			req.session.flag = true;
-			res.cookie("Auth_Cookie",User.username);
+	  		req.session.flag = true;
 	  		res.render("phone/user", {
 				username: User.username, 
 				title: "Devices", 
@@ -240,88 +232,40 @@ app.get("/logout", async (req,res) => {
 });
 /*/login system end/*/
 
-app.get("/reviews", async (req, res) => {
-	//console.log(req.cookies.Auth_Cookie);
-	let user = await userData.checkUser(req.cookies.Auth_Cookie);
-	//console.log(user);
-
-	try{
-	let allReviews = await reviews.getAllReview()
-	//console.log(user.username);
-	res.render("reviews",{
-		title: "Review page",
-		username: user.username,
-		posts: allReviews
-	});
-	console.log("rendered reviews")
-	}catch(e){
-		console.log(e);
-	}
+app.get('/login', async (req, res) => {
+	res.render('phone/login');
 });
-
-app.post("/reviews/newReview", async (req, res) => {
-
-	let user = await userData.checkUser(req.cookies.Auth_Cookie);
-	//let name = req.body.username;
-	//console.log("the name is" + name);
-	let postTitle = req.body.postTitle;
-	let postContent = req.body.postContent;
-
-	try{
-		let addedPost = await reviews.createReviews(postTitle,user.username,postContent);
-		res.status(200).end();
-	}catch(e){
-		console.log("There was an error! " + e);
-		res.status(400).end();
-	}
-
-});
-
-app.post("/reviews/removeReview", async (req, res) => {
-	let review = req.body.postId;
-	let id = ObjectId(review);
-	//console.log(typeof id);
-
-	try{
-		var removeReview = await reviews.deleteReview(id);
-		res.status(200).end();
-	}catch(e){
-		console.log("There was an error! " + e);
-		res.status(400).end();
-	}
-
-});
-
-// app.get('/login', async (req, res) => {
-// 	res.render('phone/login');
-// });
-
-// app.get('/search', async (req, res) => {
-// 	res.render('phone/homepage');
-// })
-
-// app.use("*",async (req,res)=>{
-// 	res.status(404).json({error: "Page Not Found"});
-// });
-//calculates the stars per user
-
-
 
 configRoutes(app);
 
-app.listen(3000, async() => {
-	// Run node command to create database
-	setTimeout(() => {
-		exec("node ./data/phonedb.js", (err, stdout, stderr) => {
-			if(!err){
-			console.log('sucess', stdout)
+function isDatabase(db) {
+	return db.name === 'Gadget_Clear';
+}
+app.listen(3000, async () => {
+	/// create a connection to the DB    
+	var connection = mongoose.createConnection('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true });
+
+	connection.on('open', function () {
+		// Connection established
+		new Admin(connection.db).listDatabases(function (err, result) {
+			// Database list stored in result.databases
+			var allDatabases = result.databases;
+
+			var found = allDatabases.find(isDatabase);
+
+			if (typeof found === 'undefined') {
+				setTimeout(() => {
+					exec("node ./data/phonedb.js", (err, stdout, stderr) => {
+						if (!err) {
+							console.log('sucess', stdout)
+						} else {
+							console.log(err);
+						}
+					})
+				}, 300);
 			}
-			else{
-				console.log(err);
-			}
-		})
-	}, 300);  
+		});
+	});
 	console.log("We've now got a server!");
 	console.log('Your routes will be running on http://localhost:3000');
 });
-
